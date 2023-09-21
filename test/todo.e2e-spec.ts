1d0fd18e013a0235ed5cf76174fb07ec5820e765
Jeanings Ng <jeanings.ng@gmail.com>
2023-09-21 15:57:50 -0700

    Update e2e tests

--- test/todo.e2e-spec.ts
+++ test/todo.e2e-spec.ts
@@ -1,23 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { TodoModule } from './../src/todo/todo.module';
-import { TodoController } from './../src/todo/todo.controller';
-import { TodoService } from './../src/todo/todo.service';
+import { AppModule } from '../src/app.module';
+
 
 describe("TodoController (e2e)", () => {
     let app: INestApplication;
-    const testTodoId: string = 'testId';
-    const testTodo = {
-        id: testTodoId,
-        title: 'Test todo',
-        date: null,
-        tasks: ['test']
+
+    const newId: string = "650cbdcbcfa8c6d039dbd4bc";
+    const newTodo = {
+        "title": "New todo",
+        "date": "2023-09-22T07:00:00.000Z",
+        "tasks": "Add me please"
     };
+    const deleteId = "650923396d4e4712df326ac2";
+    const deleteRequest = { "id": "650923396d4e4712df326ac2" };
+    const deleteResponse = {
+        id: '650923396d4e4712df326ac2',
+        title: 'Garbage day',
+        tasks: [ 'trash', 'organics' ],
+        completed: false
+    };
+
 
     beforeEach(async() => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [TodoModule]
+            imports: [AppModule]
         }).compile();
 
         app = moduleFixture.createNestApplication();
@@ -27,9 +35,9 @@ describe("TodoController (e2e)", () => {
     it("todo/create (POST) with expected 201 code, response", async() => {
         const response = await request(app.getHttpServer())
             .post('/todo/create')
-            // .send(testTodo)
+            .set("Accept", "application/json")
+            .send(newTodo)
             .expect(201)
-            .expect('collection with new todo');
         return response;
     });
 
@@ -37,24 +45,25 @@ describe("TodoController (e2e)", () => {
         const response = await request(app.getHttpServer())
             .get('/todo/getAll')
             .expect(200)
-            .expect('entire collection');
         return response;
     });
 
     it("todo/update (PATCH) with expected 200 code, response", () => {
         const response = request(app.getHttpServer())
-            .patch(`/todo/update/${testTodo.id}`)
-            // .send(testTodo)
+            .patch(`/todo/update/${newId}`)
+            .set("Accept", "application/json")
+            .send(newTodo)
             .expect(200)
-            .expect(`collection with updated todo ${testTodo.id}`);
         return response;
     });
 
-    it("todo/delete (DELETE) with correct 204 code, response", () => {
+    it("todo/delete (DELETE) with correct 200 code, response", () => {
         const response = request(app.getHttpServer())
-            .delete(`/todo/delete/${testTodo.id}`)
-            .expect(204)
-            .expect('');
+            .delete(`/todo/delete/${deleteId}`)
+            .set("Accept", "application/json")
+            .send(deleteRequest)
+            .expect(200)
+            .expect(deleteResponse);
         return response;
     });
 
